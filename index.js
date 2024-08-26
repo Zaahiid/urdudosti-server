18cd3fb2a53f9e9ead44bbffc767633b9a95b18f
Zaahiid <zahidrsheikh01@gmail.com>
2024-08-26 22:27:29 +0530

    Modified code to ES6 and added local DB

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const express = require('express')
-const bodyParser = require('body-parser');
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
 
-const User = require('./models/User');
-const app = express()
+import User from "./models/User.js";
+
+import ghazalRoutes from "./routes/ghazal.routes.js";
+
+const app = express();
 const port = process.env.PORT || 8000;
+console.log(process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Connected 😍"))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("MongoDB Connected 😍"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 
-app.post('/api/v1/user', (req, res) => {
+app.use("/api/v1/ghazal", ghazalRoutes);
+
+app.post("/api/v1/user", (req, res) => {
   const user = new User(req.body);
-  user.save()
-    .then(user => res.json(user))
-    .catch(err => res.status(400).json('Error: ' + err));
+  user
+    .save()
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
-app.get('/api/v1/users', (req, res) => {
+app.get("/api/v1/users", (req, res) => {
   User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-app.get('/', (req, res) => {
-  res.send('Server for Urdu Dosti 😍🥳 ')
-})
+app.get("/", (req, res) => {
+  res.send("Server for Urdu Dosti 😍🥳 ");
+});
 
-app.get('/about', (req, res) => {
-  res.send('About Page, 😍🥳 ')
-})
+app.get("/about", (req, res) => {
+  res.send("About Page, 😍🥳 ");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port} 😎`)
-})
+  console.log(`Example app listening on port ${port} 😎`);
+});
---
+++ routes/ghazal.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/", (req, res) => {
+    try {
+
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+export default router
